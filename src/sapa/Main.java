3ebc67112fedf33cab931195c2320c0af2e71d1e
Jaime Sola <j.sola2@gmail.com>
2020-12-18 10:12:49 +0100

    Hello 2

--- src/sapa/Main.java
+++ src/sapa/Main.java
@@ -3,7 +3,7 @@ package sapa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello student");
 	// write your code here
     }
 }
